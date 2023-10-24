99bb8a56a738041bb50c27e91cb592eb1b430b74
Dielson Sales <dielsonscarvalho@gmail.com>
2023-10-24 08:34:59 -0300

    Code to write to a spreadsheet

--- create_spreadsheet.py
+++ create_spreadsheet.py
@@ -23,19 +23,20 @@ df.dropna(subset=['reviewer_state'], inplace=True)
 
 
 # Create the data frame estados with the field 'name':
-estados = pd.DataFrame(columns=['name'])
+estados_data = []
+estados_ids = set()
 
 # Create the data frame tempos with the fields 'id', 'ano', 'dia_ano', 'dia_semana' and 'timestamp':
-tempos = pd.DataFrame(columns=['id', 'ano', 'dia_ano', 'dia_semana', 'timestamp'])
+tempos_data = [] #pd.DataFrame(columns=['id', 'ano', 'dia_ano', 'dia_semana', 'timestamp'])
 
 # Create the data frame usuarios with the fields 'id', 'faixa_etaria' and 'genero':
-usuarios = pd.DataFrame(columns=['id', 'faixa_etaria', 'genero'])
-
+usuarios_data = [] #pd.DataFrame(columns=['id', 'faixa_etaria', 'genero'])
+usuarios_ids = set()
 # Create the data frame produtos with the fields 'id', 'nome', 'marca', 'categoria' and 'subcategoria':
-produtos = pd.DataFrame(columns=['id', 'nome', 'marca', 'categoria', 'subcategoria'])
-
+produtos_data = [] #pd.DataFrame(columns=['id', 'nome', 'marca', 'categoria', 'subcategoria'])
+product_ids = set()
 # Create the data frame t_facts with the fields 'id', 'avaliacao', 'recomenda', 'estado_name', 'tempo_id', 'usuario_id' and 'produto_id':
-t_facts = pd.DataFrame(columns=['id', 'avaliacao', 'recomenda', 'estado_name', 'tempo_id', 'usuario_id', 'produto_id'])
+t_facts_data = [] #pd.DataFrame(columns=['id', 'avaliacao', 'recomenda', 'estado_name', 'tempo_id', 'usuario_id', 'produto_id'])
 
 
 # Separa entre 3 faixas etárias: "desconhecida", "jovem", "adulto" e "idoso".
@@ -68,34 +69,37 @@ with Bar("Inserting...", max=len(df)) as bar:
 
     for index, row in df.iterrows():
         reviewer_state = row['reviewer_state']
-        if reviewer_state not in estados['name'].values:
-            estado = pd.DataFrame({'name': [reviewer_state]})
-            estados = estados.append(estado, ignore_index=True)
+        if reviewer_state not in estados_ids:
+            estados_data.append({'name': reviewer_state})
+            estados_ids.add(reviewer_state)
 
-        tempo = pd.DataFrame({'id': [time_id_counter], 'ano': [row['submission_date'].year], 'dia_ano': [row['submission_date'].dayofyear], 'dia_semana': [day_of_week_map[row['submission_date'].weekday()]], 'timestamp': [row['submission_date']]})
-        tempos = tempos.append(tempo, ignore_index=True)
+        tempos_data.append({'id': time_id_counter, 'ano': row['submission_date'].year, 'dia_ano': row['submission_date'].dayofyear, 'dia_semana': day_of_week_map[row['submission_date'].weekday()], 'timestamp': row['submission_date']})
 
-        usuario = usuarios.loc[usuarios['id'] == row['reviewer_id']]
-        if usuario.empty:
-           usuario = pd.DataFrame({'id': [row['reviewer_id']], 'faixa_etaria': [calcula_faixa_etaria(row['reviewer_birth_year'])], 'genero': [row['reviewer_gender']]})
-           usuarios = usuarios.append(usuario, ignore_index=True)
-        
-        produto = produtos.loc[produtos['id'] == row['product_id']]
-        if produto.empty:
-            produto = pd.DataFrame({'id': [row['product_id']], 'nome': [row['product_name']], 'marca': [row['product_brand']], 'categoria': [row['site_category_lv1']], 'subcategoria': [row['site_category_lv2']]})
-            produtos = produtos.append(produto, ignore_index=True)
-        
-        t_fact = pd.DataFrame({'id': [time_id_counter], 'avaliacao': [row['overall_rating']], 'recomenda': [row['recommend_to_a_friend']], 'estado_name': [estado['name']], 'tempo_id': [tempo['id']], 'usuario_id': [usuario['id']], 'produto_id': [produto['id']]})
-        t_facts = t_facts.append(t_fact, ignore_index=True)
+        reviewer_id = row['reviewer_id']
+        if reviewer_id not in usuarios_ids:
+            usuarios_data.append({'id': reviewer_id, 'faixa_etaria': calcula_faixa_etaria(row['reviewer_birth_year']), 'genero': row['reviewer_gender']})
+            usuarios_ids.add(reviewer_id)
+
+        product_id = row['product_id']
+        if product_id not in product_ids:
+            produtos_data.append({'id': product_id, 'nome': row['product_name'], 'marca': row['product_brand'], 'categoria': row['site_category_lv1'], 'subcategoria': row['site_category_lv2']})
+            product_ids.add(product_id)
+
+        t_facts_data.append({'id': time_id_counter, 'avaliacao': row['overall_rating'], 'recomenda': row['recommend_to_a_friend'], 'estado_name': reviewer_state, 'tempo_id': time_id_counter, 'usuario_id': row['reviewer_id'], 'produto_id': row['product_id']})
 
         time_id_counter += 1
         bar.next()
 
 
+estados = pd.DataFrame(estados_data)
+tempos = pd.DataFrame(tempos_data)
+usuarios = pd.DataFrame(usuarios_data)
+produtos = pd.DataFrame(produtos_data)
+t_facts = pd.DataFrame(t_facts_data)
 
-
-estados.to_excel('B2W-Reviews01.xlsx', sheet_name='Estados', index=False)
-tempos.to_excel('B2W-Reviews01.xlsx', sheet_name='Tempos', index=False)
-usuarios.to_excel('B2W-Reviews01.xlsx', sheet_name='Usuarios', index=False)
-produtos.to_excel('B2W-Reviews01.xlsx', sheet_name='Produtos', index=False)
-t_facts.to_excel('B2W-Reviews01.xlsx', sheet_name='T_Facts', index=False)
+with pd.ExcelWriter('B2W-Reviews01.xlsx') as writer:
+    estados.to_excel(writer, sheet_name='Estados', index=False)
+    tempos.to_excel(writer, sheet_name='Tempos', index=False)
+    usuarios.to_excel(writer, sheet_name='Usuarios', index=False)
+    produtos.to_excel(writer, sheet_name='Produtos', index=False)
+    t_facts.to_excel(writer, sheet_name='T_Facts', index=False)
